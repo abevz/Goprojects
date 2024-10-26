57659ad77e1fd015afc0d49e2cf1e3c71554a620
Aleksey Bevz <aleksey.bevz@gmail.com>
2024-10-26 17:15:52 +0300

    Test lazygit

--- test/main.go
+++ test/main.go
@@ -1,19 +1,19 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"os"
 )
 
-// main function    
-func main()  {
-	a:= 2
-	b:= 2
-  m:= []string{"aa"}
-	c:= 0 / len(m)
-  
+// main function  
+func main() {
+	a := 2
+	b := 2
+	m := []string{"aa"}
+	c := 0 / len(m)
+
 	fmt.Println("FOO:", os.Getenv("FOO"))
-  fmt.Println("BAR:", os.Getenv("Bar"))
+	fmt.Println("BAR:", os.Getenv("Bar"))
 	fmt.Println(c)
 	fmt.Println(len(m))
 	fmt.Println("a is smaller than b" + fmt.Sprint(c))
@@ -22,18 +22,17 @@ func main()  {
 		fmt.Println("a is smaller than b")
 	} else if a == 3 {
 		fmt.Printf("kkk")
-	} 
+	}
 
 	if a > b {
 		fmt.Println("a is larger than b")
 	}
 
-  if a == b {
+	if a == b {
 		fmt.Println("a is equal to b")
 	}
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
-
 }
